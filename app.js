bbecd549ecf4665b1544639960537e5fafa5f3f6
owenkx <owenkxia@hotmail.com>
2013-04-23 19:47:20 -0700

    Update app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+function request(url, data, callback){
+  var request = $.ajax({
+    type: "GET",
+    url: url,
+    dataType: "jsonp",
+    data: data
+  });
+
+  request.done(function(response){
+    callback(response);
+  });
+}
+
+var url = 'http://search.twitter.com/search.json';
+var query = { q: "pizza" };
+
+request(url, query, function(data){
+  var labels = [];
+  var values = [];
+
+  $.each(data, function(key, value){
+    labels.push(data[key]['text']);
+    values.push(parseInt(data[key]['from_user_id']))
+  });
+
+  var data = {
+    labels : labels,
+    datasets : [
+      {
+        fillColor : "rgba(220,220,220,0.5)",
+        strokeColor : "rgba(220,220,220,1)",
+        data : values
+      }
+    ]
+  }
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var radarChart = new Chart(ctx).Radar(data);
+});
